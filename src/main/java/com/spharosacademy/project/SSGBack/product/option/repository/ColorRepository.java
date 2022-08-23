e5b412d2ca294f5bbee4f3c94a4e0ee23fcef4bf
mins00oo <alstn7223@gmail.com>
2022-08-23 20:53:28 +0900

    security

--- src/main/java/com/spharosacademy/project/SSGBack/product/option/repository/ColorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spharosacademy.project.SSGBack.product.option.repository;
-
-import com.spharosacademy.project.SSGBack.product.option.entity.Colors;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ColorRepository extends JpaRepository<Colors, Long> {
-}
--- src/main/java/com/spharosacademy/project/SSGBack/security/controller/IndexController.java
+++ src/main/java/com/spharosacademy/project/SSGBack/security/controller/IndexController.java
@@ -1,15 +1,72 @@
 package com.spharosacademy.project.SSGBack.security.controller;
 
+import com.spharosacademy.project.SSGBack.security.model.User;
+import com.spharosacademy.project.SSGBack.security.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller //view를 리턴
+@Controller // View를 리턴하겠다!!
 public class IndexController {
 
-    @GetMapping({"","/"})
+    // localhost: 8080/
+    // localhost:8080
+
+    private UserRepository userRepository;
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping({"", "/"})
     public String index() {
-        //머스테치 기본폴더 src/main/resources/
-        // 뷰리졸버 설정 : templates ~ 그냥 생략
+        // 머스테치 기본폴더 src/main/resources/
+        // 뷰리졸버 설정 : templates (prefix), mustache(suffix) 생략가
         return "index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody
+    String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody
+    String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody
+    String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/login")
+    public @ResponseBody
+    String login() {
+        return "login";
+    }
+
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "loginForm";
+    }
+
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "joinForm";
+    }
+
+    @PostMapping("/join")
+    public String join(User user) {
+        System.out.println(user);
+        user.setRole("ROLE_USER");
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        userRepository.save(user); // 회원가입 잘됨 => 비밀번호 1234 => 시큐리티로 로그인을 할 수 없다 => 이유는 패스워드가 암호화가 안되어있다.
+        return "redirect:/loginForm";
     }
 }
