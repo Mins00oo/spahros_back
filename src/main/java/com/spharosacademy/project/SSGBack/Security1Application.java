42e6ad1de944ebae100d913afa1281812370735f
mins00oo <alstn7223@gmail.com>
2022-08-24 09:22:48 +0900

    commit

--- src/main/java/com/spharosacademy/project/SSGBack/Security1Application.java
+++ src/main/java/com/spharosacademy/project/SSGBack/Security1Application.java
@@ -1,4 +1,4 @@
-package com.spharosacademy.project.SSGBack.security.security1;
+package com.spharosacademy.project.SSGBack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
